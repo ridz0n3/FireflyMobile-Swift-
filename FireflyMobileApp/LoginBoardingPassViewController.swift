fbb46f74e3c62d37ec935092f33c642d8686e427
ME-Tech Mac User 1 <zulmzmz@MockingBird.local>
2016-02-25 10:03:58 +0800

    update

--- FireflyMobileApp/LoginBoardingPassViewController.swift
+++ FireflyMobileApp/LoginBoardingPassViewController.swift
@@ -29,56 +29,77 @@ class LoginBoardingPassViewController: CommonListViewController {
         }else{
             let bookingList = listBooking[indexPath.row] as! NSDictionary
             
-            showHud("open")
-            FireFlyProvider.request(.RetrieveBoardingPass(signature, bookingList["pnr"] as! String, bookingList["departure_station_code"] as! String, bookingList["arrival_station_code"] as! String, userId), completion: { (result) -> () in
-                switch result {
-                case .Success(let successResult):
-                    do {
-                        let json = try JSON(NSJSONSerialization.JSONObjectWithData(successResult.data, options: .MutableContainers))
-                        
-                        if  json["status"].string == "success"{
+            let userInfo = defaults.objectForKey("userInfo")
+            var userList = Results<UserList>!()
+            userList = realm.objects(UserList)
+            
+            let mainUser = userList.filter("userId == %@",userInfo!["username"] as! String)
+            let mainPNR = mainUser[0].pnr.filter("pnr == %@", bookingList["pnr"] as! String)
+            
+            if mainPNR.count != 0{
+                
+                let boardingPass = mainPNR[0].boardingPass
+                
+                let storyboard = UIStoryboard(name: "BoardingPass", bundle: nil)
+                let boardingPassDetailVC = storyboard.instantiateViewControllerWithIdentifier("BoardingPassDetailVC") as! BoardingPassDetailViewController
+                boardingPassDetailVC.boardingList = boardingPass
+                boardingPassDetailVC.isOffline = true
+                self.navigationController!.pushViewController(boardingPassDetailVC, animated: true)
+                
+            }else{
+                
+                showHud("open")
+                FireFlyProvider.request(.RetrieveBoardingPass(signature, bookingList["pnr"] as! String, bookingList["departure_station_code"] as! String, bookingList["arrival_station_code"] as! String, userId), completion: { (result) -> () in
+                    switch result {
+                    case .Success(let successResult):
+                        do {
+                            let json = try JSON(NSJSONSerialization.JSONObjectWithData(successResult.data, options: .MutableContainers))
                             
-                            self.saveBoardingPass(json["boarding_pass"].arrayObject!, pnrStr: bookingList["pnr"] as! String)
-                            
-                            var i = 0
-                            var j = 0
-                            var dict = [String:AnyObject]()
-                            for info in json["boarding_pass"].arrayValue{
-                                let index = "\(j)"
-                                let imageURL = info["QRCodeURL"].stringValue
+                            if  json["status"].string == "success"{
                                 
-                                Alamofire.request(.GET, imageURL).response(completionHandler: { (request, response, data, error) -> Void in
+                                self.saveBoardingPass(json["boarding_pass"].arrayObject!, pnrStr: bookingList["pnr"] as! String)
+                                
+                                var i = 0
+                                var j = 0
+                                var dict = [String:AnyObject]()
+                                for info in json["boarding_pass"].arrayValue{
+                                    let index = "\(j)"
+                                    let imageURL = info["QRCodeURL"].stringValue
                                     
-                                    dict.updateValue(UIImage(data: data!)!, forKey: "\(index)")
-                                    i++
-                                    
-                                    if i == j{
-                                        showHud("close")
-                                        let storyboard = UIStoryboard(name: "BoardingPass", bundle: nil)
-                                        let boardingPassDetailVC = storyboard.instantiateViewControllerWithIdentifier("BoardingPassDetailVC") as! BoardingPassDetailViewController
-                                        boardingPassDetailVC.boardingPassData = json["boarding_pass"].arrayValue
-                                        boardingPassDetailVC.imgDict = dict
-                                        self.navigationController!.pushViewController(boardingPassDetailVC, animated: true)
-                                    }
-                                })
-                                j++
+                                    Alamofire.request(.GET, imageURL).response(completionHandler: { (request, response, data, error) -> Void in
+                                        
+                                        dict.updateValue(UIImage(data: data!)!, forKey: "\(index)")
+                                        i++
+                                        
+                                        if i == j{
+                                            showHud("close")
+                                            let storyboard = UIStoryboard(name: "BoardingPass", bundle: nil)
+                                            let boardingPassDetailVC = storyboard.instantiateViewControllerWithIdentifier("BoardingPassDetailVC") as! BoardingPassDetailViewController
+                                            boardingPassDetailVC.boardingPassData = json["boarding_pass"].arrayValue
+                                            boardingPassDetailVC.imgDict = dict
+                                            self.navigationController!.pushViewController(boardingPassDetailVC, animated: true)
+                                        }
+                                    })
+                                    j++
+                                }
+                            }else{
+                                showHud("close")
+                                //showErrorMessage(json["message"].string!)
+                                showErrorMessage(json["message"].string!)
                             }
-                        }else{
-                            showHud("close")
-                            //showErrorMessage(json["message"].string!)
-                            showErrorMessage(json["message"].string!)
                         }
-                    }
-                    catch {
+                        catch {
+                            
+                        }
                         
+                    case .Failure(let failureResult):
+                        showHud("close")
+                        showErrorMessage(failureResult.nsError.localizedDescription)
                     }
                     
-                case .Failure(let failureResult):
-                    showHud("close")
-                    showErrorMessage(failureResult.nsError.localizedDescription)
-                }
-                
-            })
+                })
+
+            }
         }
         
         
